a296db02e8de1b4d0cb1aaa4b0bd287a880c05c4
Felix Godfree <felixfgodfree@gmail.com>
2018-09-09 17:06:41 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,6 @@
 def position_taken?(board, index)
   if board[index] == " " || "" || nil 
     false
-  elsif board[index] != "X" || "O"
-    false
   elsif board[index] == "X" || "O"
     true
   end
